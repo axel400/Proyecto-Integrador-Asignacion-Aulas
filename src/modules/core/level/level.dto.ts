3c265027ce9effdfa3b12b9e5230ccee82831ddb
Liseth Ponce <lka.ponce@yavirac.edu.ec>
2022-07-19 20:05:01 -0500

    repository

--- src/modules/core/level/level.dto.ts
+++
@@ -1,8 +0,0 @@
-import {IsString,} from 'class-validator';
-import { PartialType } from '@nestjs/mapped-types';
-
-export class CreateLevelDto{
-    @IsString()
-    readonly name: string;
-}
-export class UpdateLevelDto extends PartialType(CreateLevelDto){}
