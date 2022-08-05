520c8c3232c26ca047a9d9917b2c18fc84a3131c
Liseth Ponce <lka.ponce@yavirac.edu.ec>
2022-08-04 21:50:37 -0500

    limpieza

--- src/assignment-classroom/day/dtos/days.dto.ts
+++
@@ -1,9 +0,0 @@
-import {IsString,} from 'class-validator';
-import { PartialType } from '@nestjs/mapped-types';
-
-export class CreateDayDto{
-    @IsString()
-    readonly name: string;
-}
-
-export class UpdateDayDto extends PartialType(CreateDayDto){}
--- src/assignment-classroom/school-year/dtos/schoolYear.dto.ts
+++
@@ -1,9 +0,0 @@
-import {IsString,} from 'class-validator';
-import { PartialType } from '@nestjs/mapped-types';
-
-export class CreateSchoolYearDto{
-    @IsString()
-    readonly name: string;
-}
-
-export class UpdateSchoolYearDto extends PartialType(CreateSchoolYearDto){}
